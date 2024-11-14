6d4f32da1975944d7175cd22e4288fd1a524d78c
riasad-alvi <alvi101.cs@gmail.com>
2024-11-14 23:57:27 +0600

    added all types of object methods

--- 1. JS Basics/1.practice.js
+++ 1. JS Basics/1.practice.js
@@ -4,3 +4,7 @@ console.log(a);
 let b = [1, 2, 3, 4, 5];
 console.log(b);
 console.log("Hello JS :) ");
+
+var canvas = document.getElementById("myCanvas");
+
+console.log(canvas);
---
+++ 1. JS Basics/temp.cpp
@@ -0,0 +1,18 @@
+/*
+
+And that each person will only have what they endeavoured towards , [53:39]
+
+and that ˹the outcome of˺ their endeavours will be seen ˹in their record˺ , [53:40]
+
+then they will be fully rewarded . [53:41]
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main()
+{
+
+    return 0;
+}
