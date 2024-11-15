9109c3320dd09102f1d2e3c10d93f807b0e67b31
riasad-alvi <alvi101.cs@gmail.com>
2024-11-15 18:16:54 +0600

    commented what each line does so far

--- 2.PathSwift/pathfinder.py
+++ 2.PathSwift/pathfinder.py
@@ -1,11 +1,17 @@
+#imports the messagebox and Tk classes from the tkinter library.
 from tkinter import messagebox , Tk
 
+#imports the pygame library, which is used for making games and handling multimedia tasks like graphics, sound, and events
 import pygame 
+
+#imports the sys library, which gives  access to system-specific functions. Here, it will be used to exit the program
 import sys
 
+#define the width and height (in pixels) of the game window
 window_width=500
 window_height=500
 
+#creates the game window using pygame. The set_mode function takes a tuple (width, height) to specify the window's dimensions
 window=pygame.display.set_mode((window_width,window_height))
 
 
